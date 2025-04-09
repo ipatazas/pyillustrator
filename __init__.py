1dc99d54cba0a61d39e805924c41e649704f753a
Isaac Robledo <isaac.robledo.martin@gmail.com>
2025-04-09 18:02:07 +0200

    v0.9.5

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from .source.plots import grid_plot, grid_plot_double
-from .source.config import DEFAULT_CONFIG
-from .source.utils import set_config, set_config_double
+from .pyillustrator.plots import grid_plot, grid_plot_double
+from .pyillustrator.config import DEFAULT_CONFIG
+from .pyillustrator.utils import set_config, set_config_double
