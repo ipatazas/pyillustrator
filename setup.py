35556209250c10d668d0c6e2cc6dcfa52028b1af
Isaac Robledo <isaac.robledo.martin@gmail.com>
2025-04-09 17:44:00 +0200

    v0.9.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyillustrator",  # Replace with your module name
-    version="0.9.0",
+    version="0.9.1",
     author="Isaac",
     author_email="isaac.robledo.martin@gmail.com",
     description="A Python module for generating various plots using Matplotlib",
